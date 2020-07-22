5101b63b6b139e5044ce18e6aff1850bf48a1480
Moslem Azad Shir Mahale <moslem.azad.shmhle@outlook.com>
2020-07-22 22:32:56 +0430

    Add project files.

---
+++ Dota2Utils/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Dota2Utils
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Bebas", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(42, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Idle";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.dragFunction);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("Bebas", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(119, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 30);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Idle";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.dragFunction);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::Dota2Utils.Properties.Resources.bg4;
+            this.pictureBox3.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(220, 150);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.dragFunction);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(222, 150);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Dota2Utils/Form1.cs
@@ -0,0 +1,214 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Dota2Utils
+{
+    public partial class Form1 : Form
+    { 
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+  
+        }
+         
+        private void AddDrag(Control Control) { Control.MouseDown += new System.Windows.Forms.MouseEventHandler(this.dragFunction); }
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IKeyboardMouseEvents m_GlobalHook = Hook.GlobalEvents(); 
+            
+            m_GlobalHook.KeyUp += M_GlobalHook_KeyUp;
+ 
+        }
+
+        Timer agiestT = new Timer();
+        Timer roshanT = new Timer();
+        private void M_GlobalHook_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.PageUp)
+            {
+                activeAgies();
+            }
+            else if (e.KeyCode==Keys.PageDown)
+            {
+                activeRosh();
+            }
+            else if (e.KeyCode == Keys.End)
+            {
+                reset();
+            }else if (e.KeyCode == Keys.Home)
+            {
+                this.TopMost = true;
+            }
+        }
+
+        bool active = false;
+        int sec = 59;
+        int min = 4;
+
+        bool Ractive = false;
+        bool RDefSpawn = false;
+        int Rsec = 59;
+        int Rmin = 10;
+
+        void reset()
+        {
+            label1.Text = "Idle";
+            label1.ForeColor = Color.White;
+            agiestT.Stop();
+            agiestT.Dispose();
+            sec = 59;
+            min = 4;
+            active = false;
+
+            label2.Text = "Idle";
+            label2.ForeColor = Color.White;
+            roshanT.Stop();
+            roshanT.Dispose();
+            Rsec = 59;
+            Rmin = 10;
+            Ractive = false;
+            RDefSpawn = false;
+
+           this.Visible = false;
+        }
+        void activeAgies()
+        {
+            if (active)
+                return;
+            active = true;
+            agiestT = new Timer();
+            agiestT.Interval = 1000;
+            agiestT.Tick += AgiestT_Tick;
+            agiestT.Start();
+            if (!Ractive)
+            {
+                activeRosh();
+            }
+            this.Visible = true;
+        }
+        void activeRosh()
+        {
+            if (Ractive)
+                return;
+            roshanT = new Timer();
+            roshanT.Interval = 1000;
+            roshanT.Tick += RoshanT_Tick;
+            Ractive = true;
+            roshanT.Start();
+            this.Visible = true;
+        }
+
+        private void RoshanT_Tick(object sender, EventArgs e)
+        {
+    
+            if (!Ractive)
+            {
+                return;
+            }
+      
+            if (Rmin <3 && RDefSpawn==false)
+            {
+                Invoke(new MethodInvoker(delegate () {
+                    label2.ForeColor = Color.Red;
+                }));
+                RDefSpawn = true;
+            }
+             
+            if (Rmin == 0 && Rsec == 0)
+            {
+                roshanT.Stop();
+                roshanT.Dispose();
+                Invoke(new MethodInvoker(delegate () {
+                    label2.Text = "DONE";
+                }));
+                active = false;
+                return;
+            }
+ 
+            Invoke(new MethodInvoker(delegate () {
+                label2.Text = Rmin.ToString("D2") + ":" + Rsec.ToString("D2");
+            }));
+
+            if (Rsec <= 0)
+            {
+                Rsec = 59;
+                Rmin--;
+            }
+            else
+            {
+                Rsec--;
+            }
+       
+        }
+
+        private void AgiestT_Tick(object sender, EventArgs e)
+        {
+          
+            if (!active)
+            {
+                return;
+            }
+            
+            if (min == 0 && sec==0)
+            {
+                agiestT.Stop();
+                agiestT.Dispose();
+                Invoke(new MethodInvoker(delegate () {
+                    label1.Text = "DONE";
+                }));
+                active = false;
+                return;
+            }
+
+            Invoke(new MethodInvoker(delegate () {
+                label1.Text = min.ToString("D2") + ":" + sec.ToString("D2");
+            }));
+
+            if (sec <=0)
+            {
+                sec = 59;
+                min--;
+            }
+            else
+            {
+                sec--;
+            }
+  
+        }
+ 
+
+        private void dragFunction(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+                // Checks if Y = 0, if so maximize the form
+                if (this.Location.Y == 0) { this.WindowState = FormWindowState.Maximized; }
+            }
+        }
+    }
+}
